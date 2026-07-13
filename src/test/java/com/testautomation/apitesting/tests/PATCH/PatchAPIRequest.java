6caa5b32b0d7825632c75e2894aa9635a5559b68
Devarajan <pavithra.devarajan@intelsat.com>
2026-07-13 21:44:37 +0530

    API RA all basic

---
+++ src/test/java/com/testautomation/apitesting/tests/PATCH/PatchAPIRequest.java
@@ -0,0 +1,141 @@
+package com.testautomation.apitesting.tests.PATCH;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.hamcrest.Matchers;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.jayway.jsonpath.JsonPath;
+import utils.FileNameConstants;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import net.minidev.json.JSONArray;
+
+import static utils.FileNameConstants.PATCH_API_REQUEST_BODY;
+import static utils.FileNameConstants.POST_API_REQUEST_BODY;
+
+public class PatchAPIRequest {
+
+    @Test
+    public void patchAPIRequest() {
+
+        try {
+
+            File file = new File(POST_API_REQUEST_BODY);
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.exists());
+            File file1 = new File(PATCH_API_REQUEST_BODY);
+            System.out.println(file1.getAbsolutePath());
+            System.out.println(file1.exists());
+
+            String postAPIRequestBody = FileUtils.readFileToString(new File(POST_API_REQUEST_BODY),"UTF-8");
+
+            String tokenAPIRequestBody = FileUtils.readFileToString(new File(FileNameConstants.TOKEN_API_REQUEST_BODY),"UTF-8");
+
+            String putAPIRequestBody = FileUtils.readFileToString(new File(FileNameConstants.PUT_API_REQUEST_BODY),"UTF-8");
+
+            String patchAPIRequestBody = FileUtils.readFileToString(new File(FileNameConstants.PATCH_API_REQUEST_BODY),"UTF-8");
+
+            //post api call
+            Response response =
+                    RestAssured
+                            .given()
+                            .contentType(ContentType.JSON)
+                            .body(postAPIRequestBody)
+                            .baseUri("https://restful-booker.herokuapp.com/booking")
+                            .when()
+                            .post()
+                            .then()
+                            .assertThat()
+                            .statusCode(200)
+                            .extract()
+                            .response();
+
+            System.out.println("POST "+response.path("booking.firstname"));
+
+            JSONArray jsonArray = JsonPath.read(response.body().asString(),"$.booking..firstname");
+            String firstName = (String) jsonArray.get(0);
+
+            Assert.assertEquals(firstName, "api testing");
+
+            int bookingId = JsonPath.read(response.body().asString(),"$.bookingid");
+
+            //get api call
+            RestAssured
+                    .given()
+                    .contentType(ContentType.JSON)
+                    .baseUri("https://restful-booker.herokuapp.com/booking")
+                    .when()
+                    .get("/{bookingId}",bookingId)
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+
+            //token generation
+            Response tokenAPIResponse =
+                    RestAssured
+                            .given()
+                            .contentType(ContentType.JSON)
+                            .body(tokenAPIRequestBody)
+                            .baseUri("https://restful-booker.herokuapp.com/auth")
+                            .when()
+                            .post()
+                            .then()
+                            .assertThat()
+                            .statusCode(200)
+                            .extract()
+                            .response();
+
+            String token = JsonPath.read(tokenAPIResponse.body().asString(),"$.token");
+
+            //put api call
+            Response rpt=RestAssured
+                    .given()
+                    .contentType(ContentType.JSON)
+                    .body(putAPIRequestBody)
+                    .header("Cookie", "token="+token)
+                    .baseUri("https://restful-booker.herokuapp.com/booking")
+                    .when()
+                    .put("/{bookingId}",bookingId)
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .body("firstname", Matchers.equalTo("PutUpdate"))
+                    .body("lastname", Matchers.equalTo("Selenium C#"))
+                    .extract()
+                    .response();
+
+            System.out.println("PUT  "+rpt.path("firstname"));
+
+            //patch api call
+            Response rp= RestAssured
+                    .given()
+                    .contentType(ContentType.JSON)
+                    .body(patchAPIRequestBody)
+                    .header("Cookie", "token="+token)
+                    .baseUri("https://restful-booker.herokuapp.com/booking")
+                    .when()
+                    .patch("/{bookingId}",bookingId)
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .body("firstname", Matchers.equalTo("PatchUpdate"))
+                    .extract()
+                    .response();
+
+            System.out.println("Patch "+rp.path("firstname"));
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
---
+++ src/test/java/com/testautomation/apitesting/utils/BaseTest.java
@@ -0,0 +1,39 @@
+package com.testautomation.apitesting.utils;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.testng.annotations.BeforeMethod;
+
+import io.restassured.RestAssured;
+
+public class BaseTest {
+
+  //  private static final Logger logger = LogManager.getLogger(BaseTest.class);
+
+    @BeforeMethod
+    public void beforeMethod() {
+        //RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+       // RestAssured.requestSpecification = RestAssured.given().log().all(); //log request details only
+
+        //to log all details of req and response use below
+        RestAssured.filters(
+                new RequestLoggingFilter(),
+                new ResponseLoggingFilter()
+        );
+    }
+
+//    @AfterMethod
+//    public void afterMethod(ITestResult result) {
+//        if(result.getStatus() == ITestResult.FAILURE) {
+//            Throwable t = result.getThrowable();
+//            StringWriter error = new StringWriter();
+//            t.printStackTrace(new PrintWriter(error));
+//            logger.info(error.toString());
+//        }
+//
+//    }
+
+}
